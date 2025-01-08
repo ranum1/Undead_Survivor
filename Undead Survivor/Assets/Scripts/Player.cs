42eedab3cd5275aeb497c535c65cc53040a6f916
VeritasRati0 <rud030705@naver.com>
2025-01-08 18:53:07 +0900

    Make Cel Animation

--- Undead Survivor/Assets/Scripts/Player.cs
+++ Undead Survivor/Assets/Scripts/Player.cs
@@ -11,12 +11,16 @@ public class Player : MonoBehaviour
     public float speed;
 
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
 
     //시작할 때 한번만 실행되는 생명주기 Awake에 실행
     void Awake()
     {
         //GetComponent<> : 오브젝트에서 컴포넌트를 가져오는 함수
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,7 +30,7 @@ public class Player : MonoBehaviour
     //FixedUpdate : 물리 연산 프레임마다 호출되는 생명주기 함수
     void FixedUpdate()
     {
-        // normalized : 벡터 값의 크기가 1이 되도록 좌표가 수정된 값'
+        // normalized : 벡터 값의 크기가 1이 되도록 좌표가 수정된 값
         // fixedDeltaTime : 물리 프레임 하나가 소비한 시간(FixedUpdate에서 사용)
         Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
         // 위치 이동 MovePosition
@@ -38,5 +42,18 @@ public class Player : MonoBehaviour
     {
         // Get<> : 프로필에서 설정한 컨트롤 타입 값을 가져오는 함수
         inputVec = value.Get<Vector2>();
+    }
+
+    // 프레임이 종료 되기 전 실행되는 생명주기 함수수
+    void LateUpdate()
+    {
+        // magnitude : 벡터의 순수한 크기 값값
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        // != : '왼쪽과 오른쪽이 서로 다릅니까?' 의미의 비교 연산자
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
     }
 }
