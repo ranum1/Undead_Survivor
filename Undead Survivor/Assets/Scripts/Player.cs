7a246b92f420dc93dda655018ea5671c975723a8
VeritasRati0 <rud030705@naver.com>
2025-01-08 18:10:41 +0900

    Apply New Input System

--- Undead Survivor/Assets/Scripts/Player.cs
+++ Undead Survivor/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -19,22 +20,23 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        // 유니티에서 받는 모든 입력을 관리하는 클래스
-        // GetAxisRaw로 더욱 명확한 컨트롤 구현 
-        inputVec.x = Input.GetAxisRaw("Horizontal");   //수평 x축
-        inputVec.y = Input.GetAxisRaw("Vertical");     //수직 y축
-    }
+
+
 
     //FixedUpdate : 물리 연산 프레임마다 호출되는 생명주기 함수
     void FixedUpdate()
     {
         // normalized : 벡터 값의 크기가 1이 되도록 좌표가 수정된 값'
         // fixedDeltaTime : 물리 프레임 하나가 소비한 시간(FixedUpdate에서 사용)
-        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
         // 위치 이동 MovePosition
         // MovePosition은 위치 이동이라 현재 위치도 더해주어야함
         rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void OnMove(InputValue value)
+    {
+        // Get<> : 프로필에서 설정한 컨트롤 타입 값을 가져오는 함수
+        inputVec = value.Get<Vector2>();
     }
 }
